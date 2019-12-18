a4c2eb43d9fda1dd91395422deca89ab69011017
Jack OMalley <jfo.omalley@gmail.com>
2019-12-18 13:32:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,14 @@ class Student
   #  with DB[:conn]
 
   attr_accessor :name, :grade
+  
   attr_reader :id
+
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
     @album = album
   end
+
+
 end
